1da0df47725a9924f4ace067557dedaa1508d1c7
Kyle Whitley <kwhitley11@gmail.com>
2024-01-29 18:08:39 -0500

    added tasks 1 - 6 in final project

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,15 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
+    1: {"author": "Chinua Achebe", "title": "Things Fall Apart", "isbn": "9780385474542", "reviews": {"John": "Captivating story, couldn't put it down!"} },
+    2: {"author": "Hans Christian Andersen", "title": "Fairy tales", "isbn": "9780141439601", "reviews": {"Alice": "A masterpiece of suspense and intrigue."} },
+    3: {"author": "Dante Alighieri", "title": "The Divine Comedy", "isbn": "9780142437223", "reviews": {"Emma": "Heartwarming tale, perfect for a cozy evening."} },
+    4: {"author": "Unknown", "title": "The Epic Of Gilgamesh", "isbn": "9780141026282", "reviews": {"David": "Incredible characters, beautifully written."} },
+    5: {"author": "Unknown", "title": "The Book Of Job", "isbn": "9780140444513", "reviews": {"Sophia": "Unexpected twists kept me hooked till the end."} },
+    6: {"author": "Unknown", "title": "One Thousand and One Nights", "isbn": "9780679413382", "reviews": {"Olivia": "A delightful escape into a vivid world."} },
+    7: {"author": "Unknown", "title": "Nj\u00e1l's Saga", "isbn": "9780140447767", "reviews": {"Liam": "Poignant and thought-provoking."} },
+    8: {"author": "Jane Austen", "title": "Pride and Prejudice", "isbn": "9780141439519", "reviews": {"Emma": "Page-turner with a satisfying conclusion."} },
+    9: {"author": "Honor\u00e9 de Balzac", "title": "Le P\u00e8re Goriot", "isbn": "9780140440416", "reviews": {"Noah": "Lyrical prose, deeply moving."} },
+    10: {"author": "Samuel Beckett", "title": "Molloy, Malone Dies, The Unnamable, the trilogy", "isbn": "9780802144393", "reviews": {"Isabella": "A classic that never gets old."} }
+};
+
 
 module.exports=books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,39 +5,98 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
+function getBookDetailsByISBN(isbn) {
+    for (let key in books) {
+        if (books[key].isbn === isbn) {
+            return books[key];
+        }
+    }
+    return null; // Return null if no book with matching ISBN is found
+}
+
+function getBookDetailsByAuthor(author) {
+    for (let key in books) {
+        if (books[key].author === author) {
+            return books[key];
+        }
+    }
+    return null; // Return null if no book with matching author is found
+}
+
+function getBookDetailsByTitle(title) {
+    for (let key in books) {
+        if (books[key].title === title) {
+            return books[key];
+        }
+    }
+    return null; // Return null if no book with matching title is found
+}
+
+function getBookReviewsByISBN(isbn) {
+    for (let key in books) {
+        if (books[key].isbn === isbn) {
+            return books[key].title + ' ' + JSON.stringify(books[key].reviews);
+        }
+    }
+    return null; // Return null if no book with matching ISBN is found
+}
+
+let userList = []
+
+const doesExist = (username)=>{
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.query.username;
+    const password = req.query.password;
+  
+    if (username && password) {
+      if (!doesExist(username)) { 
+        userList.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    const isbn = req.params.isbn;
+    res.send(getBookDetailsByISBN(isbn));
+})
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    res.send(getBookDetailsByAuthor(author));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    res.send(getBookDetailsByTitle(title));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(getBookReviewsByISBN(isbn));
 });
 
 module.exports.general = public_users;
