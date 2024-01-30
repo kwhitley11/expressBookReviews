9c8dbc8a0b7c2e50c3dc2d80b3c8000944c6bbda
Kyle Whitley <kwhitley11@gmail.com>
2024-01-29 19:35:37 -0500

    updating to stopping point

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -41,7 +41,6 @@ function getBookReviewsByISBN(isbn) {
     return null; // Return null if no book with matching ISBN is found
 }
 
-let userList = []
 
 const doesExist = (username)=>{
   let userswithsamename = users.filter((user)=>{
@@ -61,7 +60,7 @@ public_users.post("/register", (req,res) => {
   
     if (username && password) {
       if (!doesExist(username)) { 
-        userList.push({"username":username,"password":password});
+        users.push({"username":username,"password":password});
         return res.status(200).json({message: "User successfully registred. Now you can login"});
       } else {
         return res.status(404).json({message: "User already exists!"});    
